857c88704fd20ee3563ebad6f076def5946f4892
Mostafa Azab <110853054+MostafaAzab30@users.noreply.github.com>
2024-05-26 22:31:42 +0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,112 @@
+
+
+
+
+
+
+
+const kill = document.getElementById("kill")
+
+
+
+// const smaler = document.querySelectorAll(".smaler")
+
+
+
+//     smaler.forEach((eo) => {
+    
+//         eo.addEventListener("click" , () => {
+
+//             dis1.style['opacity'] = '1';
+
+        
+//     })
+
+// });
+
+
+
+dis.addEventListener("click" , () => {
+
+    dis1.style['display'] = 'block';
+
+
+})
+
+hido.addEventListener("click" , () => {
+
+    dis2.style['display'] = 'block';
+    dis3.style['display'] = 'block';
+
+})
+
+zer.addEventListener("click" , () => {
+
+    dis2.style['display'] = 'block';
+    dis3.style['display'] = 'block';
+
+})
+
+zer1.addEventListener("click" , () => {
+
+    dis2.style['display'] = 'block';
+    dis3.style['display'] = 'block';
+
+})
+
+hido1.addEventListener("click" , () => {
+
+    dis4.style['display'] = 'block';
+
+
+})
+
+hido2.addEventListener("click" , () => {
+
+    dis4.style['display'] = 'block';
+
+
+})
+
+
+
+
+$( function() {
+    $( ".draggable" ).draggable();
+    } );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
